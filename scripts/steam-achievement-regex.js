68b1db3214f4dc117e829315338b6da702a92320
Chua Hou <human+github@chuahou.dev>
2021-05-06 00:23:54 +0800

    steam-achievement-regex: add locked/unlocked/all option

--- scripts/steam-achievement-regex.js
+++ scripts/steam-achievement-regex.js
@@ -1,18 +1,43 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2021 Chua Hou
 
-function filterAchievements(regex) {
+// Insert into DOM.
+const select = document.createElement("SELECT");
+["all", "locked", "unlocked"].forEach(x => {
+	const option = document.createElement("OPTION");
+	option.value = x; option.innerText = x;
+	select.appendChild(option);
+});
+const input = document.createElement("INPUT");
+const extras = document.createElement("DIV");
+extras.appendChild(input); extras.appendChild(select);
+const headerContent = document.querySelector("#headerContent");
+headerContent.parentNode.insertBefore(extras, headerContent);
+
+// Actual filtering function.
+function filterAchievements(regex, states) {
 	const rows = Array.from(document.querySelectorAll(".achieveRow"));
 	rows.forEach(row => row.style.display = ""); // reset previous runs
-	const nonMatches = rows.filter(x =>
-		!x.querySelector(".achieveTxt").innerText.match(regex));
-	nonMatches.forEach(row => row.style.display = "none");
+	rows.filter(row =>
+			!row.querySelector(".achieveTxt").innerText.match(regex))
+		.forEach(row => row.style.display = "none");
+	const isUnlocked = x => x.classList.contains("unlocked");
+	switch (String(states)) {
+		case "unlocked":
+			rows.filter(x => !isUnlocked(x)).forEach(row =>
+				row.style.display = "none");;
+			break;
+		case "locked":
+			rows.filter(x => isUnlocked(x)).forEach(row =>
+				row.style.display = "none");;
+			break;
+		case "all":
+		default:
+			break;
+	}
 }
 
-const input = document.createElement("INPUT");
-input.addEventListener("change", x => {
-	filterAchievements(new RegExp(x.currentTarget.value));
-});
-
-const headerContent = document.querySelector("#headerContent");
-headerContent.parentNode.insertBefore(input, headerContent);
+// Event listener.
+const update = () => filterAchievements(new RegExp(input.value), select.value);
+input.addEventListener("input", update);
+select.addEventListener("change", update);
